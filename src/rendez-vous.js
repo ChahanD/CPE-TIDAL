8326d2b37dd3b6d643a1e5536bf90f14040e5907
UrocyonF <max02smart@gmail.com>
2024-04-25 17:43:23 +0200

    auth n rdv page twiged & img n style get from main

---
+++ src/rendez-vous.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', function() {
+    function generateRandomTimes(date) {
+      let times = [];
+      for (let i = 0; i < 3; i++) {
+            let hour = Math.floor(Math.random() * 8) + 9;
+            let minute = Math.floor(Math.random() * 2) * 30;
+            let formattedMinute = minute < 10 ? '0' + minute : minute;
+            times.push({ hour, minute, text: `${hour}h${formattedMinute}` });
+        }
+      return times;
+    }
+
+    let events = [];
+    for (let i = 0; i < 30; i++) {
+        let eventDate = new Date();
+        eventDate.setDate(eventDate.getDate() + i);
+        let eventTimes = generateRandomTimes(eventDate);
+
+        eventTimes.forEach(time => {
+            events.push({
+            title: time.text,
+            start: new Date(eventDate.getFullYear(), eventDate.getMonth(), eventDate.getDate(), time.hour, time.minute),
+            end: new Date(eventDate.getFullYear(), eventDate.getMonth(), eventDate.getDate(), time.hour + 1, time.minute),
+            extendedProps: {
+                availableTimes: eventTimes
+            }
+            });
+        });
+    }
+
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        selectable: true,
+        selectMirror: true,
+        eventContent: function(arg) {
+            return { html: '<input type="radio" name="appointment-time" class="fc-event-radio" value="' + arg.event.title + '">' + arg.event.title };
+        },
+        eventClick: function(info) {
+            let selectedTime = info.el.querySelector('.fc-event-radio');
+            if (selectedTime.checked) {
+            confirmAppointment(info.event.start, info.event.extendedProps.availableTimes);
+            }
+        },
+        events: events
+    });
+    calendar.render();
+
+    function confirmAppointment(date, times) {
+        let selectedTime = times.find(t => t.text === document.querySelector('input[name="appointment-time"]:checked').value);
+        if (selectedTime) {
+            document.getElementById('appointment-date-time').textContent = date.toLocaleString('fr-FR', { dateStyle: 'long', timeStyle: 'short' });
+            document.getElementById('appointment-form').style.display = 'block';
+        }
+    }
+
+    document.getElementById('cancel-appointment').addEventListener('click', function() {
+        document.getElementById('appointment-form').style.display = 'none';
+    });
+});
--- src/rendez-vous.php
+++ src/rendez-vous.php
@@ -4,9 +4,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/vendor/autoload.php';
+include 'database.php';
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/views');
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('authentification.html.twig', ['currentPage' => 'authentification']);
+echo $twig->render('rendez-vous.html.twig', ['currentPage' => 'rdv']);
 ?>
